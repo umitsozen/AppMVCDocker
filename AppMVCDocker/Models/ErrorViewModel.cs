0c9c80ffcbe0acfee05cb702eaca0c7d387c86e1
umitsozen <uguneri@gmail.com>
2023-08-28 23:58:33 +0300

    Add project files.

---
+++ AppMVCDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppMVCDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
